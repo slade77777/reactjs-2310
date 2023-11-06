f6b04323013504416f0d3d086c7e2593e971c06b
cuongdang <cuongdx@bfast.dev>
2023-11-06 19:58:10 +0700

    styled component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,26 @@
+import styled from "styled-components";
+
 const Header = () => {
     return <div>
-        Welcome to NexFlix website
+        <StyledTitle>
+            Welcome to NexFlix website
+        </StyledTitle>
+        <SubStyledTitle aColor="red">
+            Nice to see you
+        </SubStyledTitle>
+        <SubStyledTitle aColor="yellow">
+            Please vote for us
+        </SubStyledTitle>
     </div>
 }
 
 export default Header;
+
+const StyledTitle = styled.p`
+  font-size: 30px;
+  text-align: center;
+`
+
+const SubStyledTitle = styled(StyledTitle)<{aColor: string}>`
+    color: ${(attribute) => attribute.aColor}
+`
